ac5663a559ee047aa18d34bfc5e8b18fcc32d3cd
milcho0604 <milcho0604@gmail.com>
2025-02-04 15:15:05 +0900

    feat: java code를 활용한 구성 정보 변경 config와 bean 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -5,20 +5,35 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
+@Configuration
 public class HellobootApplication {
 
+    @Bean
+    public HelloController helloController(HelloService helloService){
+        return new HelloController(helloService);
+    }
+
+    // SimpleHelloService return은 기능적으로 문제는 없지만, 의존성 주입 즉, 인터페이스 타입이 있으니 인터페이스 타입으로 return 하는 것이 더 적합
+    @Bean
+    public HelloService helloService(){
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext genericApplicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -32,11 +47,8 @@ public class HellobootApplication {
             }
         };
 
-        genericApplicationContext.registerBean(HelloController.class); // class 정보만 넘김
-        genericApplicationContext.registerBean(SimpleHelloService.class); // class 정보만 넘김
-        genericApplicationContext.refresh();
-
-
+        applicationContext.register(HellobootApplication.class);
+        applicationContext.refresh();
 
     }
 
