2f12e4508b18902f42864a5a5b49c5fced5914bb
milcho0604 <milcho0604@gmail.com>
2025-02-04 15:48:06 +0900

    feat: Application 분리

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -24,27 +23,8 @@ public class HellobootApplication {
     public DispatcherServlet dispatcherServlet(){
         return new DispatcherServlet();
     }
+
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-                ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//                dispatcherServlet.setApplicationContext(this); // -> 어떻게 스프링 컨테이너를 통해 HelloController를 찾을까?
-//                -> 코드 상에는 dispatcherServlet applicationContext를 사용하지 않음.
-//                -> 스프링 컨테이너가 자체적으로 주입해줌
-
-                WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-
-        applicationContext.register(HellobootApplication.class);
-        applicationContext.refresh();
+        MySpringApplication.run(HellobootApplication.class, args);
     }
-
 }
---
+++ src/main/java/tobyspring/helloboot/MySpringApplication.java
@@ -0,0 +1,31 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+    public static void run(Class<?> applicationClass, String[] args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//                dispatcherServlet.setApplicationContext(this); // -> 어떻게 스프링 컨테이너를 통해 HelloController를 찾을까?
+//                -> 코드 상에는 dispatcherServlet applicationContext를 사용하지 않음.
+//                -> 스프링 컨테이너가 자체적으로 주입해줌
+
+                WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                            .addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+
+        applicationContext.register(applicationClass);
+        applicationContext.refresh();
+    }
+}
