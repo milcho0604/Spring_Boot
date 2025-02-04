4c1fbcd00cfbc9e9bb4e4e797986f97cb5899f8e
milcho0604 <milcho0604@gmail.com>
2025-02-04 15:43:36 +0900

    feat: 생성자 없이 DisPatcherServlet 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,6 +1,6 @@
 package tobyspring.helloboot;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
@@ -14,9 +14,13 @@ public class HelloController {
     // http -v GET ":8080/hello?name=micho"
 
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
     @GetMapping
@@ -24,5 +28,4 @@ public class HelloController {
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name)); // null이라면 예외 아니라면 값을 그대로 return
     }
-
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -31,7 +31,7 @@ public class HellobootApplication {
                 super.onRefresh();
                 ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
                 DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//                dispatcherServlet.setApplicationContext(this); -> 어떻게 스프링 컨테이너를 통해 HelloController를 찾을까?
+//                dispatcherServlet.setApplicationContext(this); // -> 어떻게 스프링 컨테이너를 통해 HelloController를 찾을까?
 //                -> 코드 상에는 dispatcherServlet applicationContext를 사용하지 않음.
 //                -> 스프링 컨테이너가 자체적으로 주입해줌
 
