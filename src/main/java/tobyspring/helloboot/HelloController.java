032c801054c36a07a5845575f5e96036152143e4
milcho0604 <milcho0604@gmail.com>
2025-02-04 14:59:06 +0900

    feat: Di 스프링 컨테이너

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,11 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Objects;
 
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
     // http://localhost:8080/hello?name=milcho
     // http -v ":8080/hello?name=micho" (terminal)
@@ -13,6 +17,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name)); // null이라면 예외 아니라면 값을 그대로 return
     }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -17,43 +17,24 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
 import java.io.IOException;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext genericApplicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext genericApplicationContext = new GenericWebApplicationContext();
         genericApplicationContext.registerBean(HelloController.class); // class 정보만 넘김
         genericApplicationContext.registerBean(SimpleHelloService.class); // class 정보만 넘김
         genericApplicationContext.refresh();
 
         ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
         WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-
-//            HelloController helloController = new HelloController();
-            servletContext.addServlet("frontController", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능 등을 frontController에서 처리
-                    // -> ServletContainer의 기능을 frontController에게 위임
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-                        String name = req.getParameter("name"); // name 추출은 frontController의 역할
-
-                        HelloController helloController = genericApplicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name); // return 값
-
-//                        resp.setStatus(HttpStatus.OK.value()); // 상태 코드
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE); // Header
-                        resp.getWriter().println(ret); // Body
-                    } else if (req.getRequestURI().equals("/user")) {
-                        //
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value()); // 404 던짐
-                    }
-
-                }
-            }).addMapping("/*"); // 모든 요청을 여기서 처리
-
+            servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(genericApplicationContext))
+                    .addMapping("/*");
         });
         webServer.start();
 
