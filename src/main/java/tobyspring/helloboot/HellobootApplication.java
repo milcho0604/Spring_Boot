7707129e934be2246573f0d49479d6faebcc93e6
milcho0604 <milcho0604@gmail.com>
2025-02-04 13:41:10 +0900

    feat: frontController test

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -35,16 +36,25 @@ public class HellobootApplication {
 
         ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
         WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어, 공통 기능 등을 frontController에서 처리
+                    // -> ServletContainer의 기능을 frontController가 위임
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+                        String name = req.getParameter("name");
 
-                    resp.setStatus(HttpStatus.OK.value()); // 상태 코드
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Header
-                    resp.getWriter().println("Hello " + name); // Body
+                        resp.setStatus(HttpStatus.OK.value()); // 상태 코드
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // Header
+                        resp.getWriter().println("Hello " + name); // Body
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value()); // 404 던짐
+                    }
+
                 }
-            }).addMapping("/hello"); // "/hello"로 들어오는 요청을 여기서 처리함.
+            }).addMapping("/*"); // 모든 요청을 여기서 처리
 
         });
 
