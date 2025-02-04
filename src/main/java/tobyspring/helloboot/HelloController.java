f46c882abd40ec8359ba7e6b6feef5bd33bc2eb5
milcho0604 <milcho0604@gmail.com>
2025-02-04 14:15:09 +0900

    feat: code 분리

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,14 +3,19 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 public class HelloController {
     // http://localhost:8080/hello?name=milcho
     // http -v ":8080/hello?name=micho" (terminal)
     // http -v GET ":8080/hello?name=micho"
 
     public String hello(String name){
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name)); // null이라면 예외 아니라면 값을 그대로 return
     }
 
+//        if (name = null) throw
 
 }
