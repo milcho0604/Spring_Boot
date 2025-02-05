05902c0cb080d1f8e77d3308ce8380096a2455e2
milcho0604 <milcho0604@gmail.com>
2025-02-05 09:09:27 +0900

    feat: JUnitTest용

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -8,6 +8,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@interface FastUnitTest{
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest{
+
+}
 public class HelloServiceTest {
     @Test
     void hellService() {
