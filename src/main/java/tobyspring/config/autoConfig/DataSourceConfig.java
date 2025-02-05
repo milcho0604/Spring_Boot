bd3e03d78e04e67085c44c521ed96a989aa1dbc4
milcho0604 <milcho0604@gmail.com>
2025-02-05 17:27:13 +0900

    feat: jdbcTemplateTest database 생성 및 비교 테스트

--- src/main/java/tobyspring/config/autoConfig/DataSourceConfig.java
+++ src/main/java/tobyspring/config/autoConfig/DataSourceConfig.java
@@ -2,9 +2,13 @@ package tobyspring.config.autoConfig;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
@@ -14,6 +18,7 @@ import java.sql.Driver;
 
 
 @MyAutoConfiguration
+@EnableTransactionManagement
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
 public class DataSourceConfig {
@@ -42,4 +47,16 @@ public class DataSourceConfig {
         return dataSource;
     }
 
+    @Bean
+    @ConditionalOnMissingBean // 위 hikari가 없으면 아래 DB에 연결
+    @ConditionalOnSingleCandidate(DataSource.class)
+    JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+    @Bean
+    @ConditionalOnMissingBean // 위 hikari가 없으면 아래 DB에 연결
+    @ConditionalOnSingleCandidate(DataSource.class)
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource){
+        return new JdbcTransactionManager(dataSource);
+    }
 }
