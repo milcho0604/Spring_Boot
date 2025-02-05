189713942a68d834bda9925ee6a9a5002b8e8097
milcho0604 <milcho0604@gmail.com>
2025-02-05 13:18:58 +0900

    feat: jetty

--- src/main/java/tobyspring/config/autoConfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoConfig/JettyWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoConfig;
 
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +8,10 @@ import tobyspring.config.MyAutoConfiguration;
 
 //@Configuration // MetaAnnotation으로 class를 가지고 있음
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
     // Tomcat 구성 Bean
-    @Bean("tomcatWebServerFactory")
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
